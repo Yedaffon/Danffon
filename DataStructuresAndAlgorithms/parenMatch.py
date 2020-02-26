f6751958348d43f5cbea7885fbf67f7fa41f1654
Daniel <67106362@qq.com>
2020-02-26 21:43:15 +0800

    YeDaffon

---
+++ DataStructuresAndAlgorithms/parenMatch.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+# Copyright 2020 Yedaffon Author.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import re
+
+
+def paren(parse):
+    so = []
+    parse = re.findall(r"\(|\)", parse)
+    for i in parse:
+        if i == "(":
+            so.append(i)
+        elif i == ")":
+            so.pop()
+    if len(so) == 0:
+        return True
+
+
+if __name__ == '__main__':
+    parse = "((aaa)(bbb)(ccc)())"
+    s = paren(parse)
+    print(s)
