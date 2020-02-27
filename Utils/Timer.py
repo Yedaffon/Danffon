99539ca78836555ab265d296e4dc330bbaa6c75a
Daniel <67106362@qq.com>
2020-02-27 23:57:27 +0800

    YeDaffon

---
+++ Utils/Timer.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+# Copyright 2020 Yedaffon Author.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# 计时器:
+import time
+import functools
+
+
+def timer(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        res = func(*args, **kwargs)
+        end = time.time()
+        cost = end - start
+        print("耗时: {:.3f}s".format(cost))
+        return res
+    return wrapper
