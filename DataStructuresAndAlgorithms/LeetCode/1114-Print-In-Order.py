b4f7100edaad4b31598f8d0c920bfd9b029e129f
Yedaffon <bluejazz831744@gmail.com>
2020-03-26 11:04:59 +0800

    YeDaffon

---
+++ DataStructuresAndAlgorithms/LeetCode/1114-Print-In-Order.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+# Copyright 2020 Yedaffon Author.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from threading import Lock
+# 1114-按序打印
+
+
+class Foo:
+    def __init__(self):
+        self.firstJobDone = Lock()
+        self.secondJobDone = Lock()
+        self.firstJobDone.acquire()
+        self.secondJobDone.acquire()
+        pass
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+        self.firstJobDone.release()
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        # printSecond() outputs "second". Do not change or remove this line.
+        with self.firstJobDone:
+            printSecond()
+            self.secondJobDone.release()
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        # printThird() outputs "third". Do not change or remove this line.
+        with self.secondJobDone:
+            printThird()
